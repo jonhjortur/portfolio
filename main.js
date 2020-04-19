73458ed265be1019a2076c565489ada2f34cead4
Jón Hjörtur <jonhjortur@protonmail.com>
2020-04-19 12:38:04 +0000

    Make contact form work

--- main.js
+++ main.js
@@ -28,13 +28,35 @@ $(document).ready(function () {
   });
 
   /*Því að AOS er með vesen á litlum skjáum. */
-  if(screen.width < 768) {
-    $('div').attr('data-aos', 'fade-in');
+  if (screen.width < 768) {
+    $("div").attr("data-aos", "fade-in");
   }
-  
+
   AOS.init({
     easing: "ease",
     duration: 1800,
     //once: true
   });
+  /*
+  $("#contact-form").submit(function (e) {
+    var name = document.getElementById("inputName");
+    var email = document.getElementById("inputEmail");
+    var message = document.getElementById("inputMessage");
+
+    if (!name.value || !email.value || !message.value) {
+      alertify.error("Please check your entries");
+      return false;
+    } else {
+      $.ajax({
+        method: "POST",
+        url: "//formspree.io/jonhjortur@protonmail.com",
+        data: $("#contact-form").serialize(),
+        datatype: "json",
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      alertify.success("Message sent");
+    }
+  });
+  */
 });
